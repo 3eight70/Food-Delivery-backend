68bc1b6813fe751051914e42161e107c023bff34
3eight70 <gbhfns90@gmail.com>
2023-10-12 14:14:25 +0700

    Add classes

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webNET_Hits_backend_aspnet_project_1.Models;
+
+public class Order
+{
+    [Key]
+    public Guid Id { get; set; }
+    public DateTime DeliveryTime { get; set; }
+    public DateTime OrderTime { get; set; }
+    public Status status { get; set; }
+    public int Price { get; set; }
+}
