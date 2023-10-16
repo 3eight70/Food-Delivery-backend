c95465be6003881fc496c70072e73630e95e74cc
Vasilev Danil <111416100+3eight70@users.noreply.github.com>
2023-10-16 11:53:32 +0700

    add userDTO

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using webNET_Hits_backend_aspnet_project_1.Models;
+using webNET_Hits_backend_aspnet_project_1.Models.DTO;
 using webNET_Hits_backend_aspnet_project_1.Services;
 
 namespace webNET_Hits_backend_aspnet_project_1.Controllers;
@@ -17,16 +18,37 @@ public class UserController: ControllerBase
     
     [HttpGet]
     [Route("profile")]
-    public ActionResult<User> Get()
+    public ActionResult<UserDTO> Get()
     {
         return userService.GetUserProfile();
     }
 
     [HttpPost]
     [Route("logout")]
-    public ActionResult<User> Post()
+    public ActionResult<UserDTO> Logout()
     {
         return null;
+    }
+    
+    [HttpPost]
+    [Route("register")]
+    public async Task<ActionResult> Post(UserDTO model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return StatusCode(401, "User model is invalid.");
+        }
+
+        try
+        {
+            await userService.Add(model);
+            return Ok(userService.RegisterUser());
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Something went wrong with user model");
+        }
+        
     }
     
 }
